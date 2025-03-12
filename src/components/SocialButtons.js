60b01445c7ed9536e2b44913ccb52b092c5a3df2
drewdi <realandrewhunter@gmail.com>
2025-03-12 10:37:55 -0400

    update website links

--- src/components/SocialButtons.js
+++ src/components/SocialButtons.js
@@ -9,10 +9,10 @@ function SocialButtons() {
                 <a href="https://x.com/andrewhunnter" className="social-button twitter" target="_blank" rel="noopener noreferrer">
                     <span className="iconify" data-icon="simple-icons:x"></span>
                 </a>
-                <a href="https://t.me/andrewhunnter" className="social-button telegram" target="_blank" rel="noopener noreferrer">
+                <a href="https://t.me/stackedinfo" className="social-button telegram" target="_blank" rel="noopener noreferrer">
                     <span className="iconify" data-icon="mdi:telegram"></span>
                 </a>
-                <a href="https://tiktok.com/@andrewhunnter" className="social-button tiktok" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.tiktok.com/@fullstackeddd" className="social-button tiktok" target="_blank" rel="noopener noreferrer">
                     <span className="iconify" data-icon="simple-icons:tiktok"></span>
                 </a>
                 <a href="https://instagram.com/andrewhunnter" className="social-button instagram" target="_blank" rel="noopener noreferrer">
